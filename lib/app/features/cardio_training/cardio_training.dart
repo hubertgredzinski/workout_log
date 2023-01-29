6b1419d4a5d7beab5232b4451926525f8abfc34c
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-01-29 11:38:11 +0100

    Add pages strength and cardio training with appbar

---
+++ lib/app/features/cardio_training/cardio_training.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CardioTrainingPage extends StatefulWidget {
+  const CardioTrainingPage(
+    Key? key,
+  ) : super(key: key);
+
+  @override
+  State<CardioTrainingPage> createState() => _CardioTrainingPageState();
+}
+
+class _CardioTrainingPageState extends State<CardioTrainingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Cardio Training"),
+        actions: const [],
+      ),
+      body: const Text('elo'),
+    );
+  }
+}
---
+++ lib/app/features/strength training/strength_training.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class StrengthTrainingPage extends StatefulWidget {
+  const StrengthTrainingPage(
+    Key? key,
+  ) : super(key: key);
+
+  @override
+  State<StrengthTrainingPage> createState() => _StrengthTrainingPageState();
+}
+
+class _StrengthTrainingPageState extends State<StrengthTrainingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Strength Training"),
+        actions: const [],
+      ),
+      body: const Text('elo'),
+    );
+  }
+}
