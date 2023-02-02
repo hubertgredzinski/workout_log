559a4f7f7a0a58b3410376f4a9b1d5139db909fd
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-02-02 07:42:38 +0100

    Add elevatedbutton in historypage

--- lib/app/features/history/history_page.dart
+++ lib/app/features/history/history_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:workout_log/app/features/history/history_cardio_page.dart';
+import 'package:workout_log/app/features/history/history_strength_training_page.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({
@@ -7,8 +10,67 @@ class HistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Historia treningu'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Text(
+              'Choose your training',
+              style: GoogleFonts.robotoSlab(
+                  fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HistoryStrengthTrainingPage(),
+                  ),
+                );
+              },
+              child: Text(
+                'Strength Training',
+                style: GoogleFonts.robotoSlab(
+                    fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const HistoryCardioTrainingPage(),
+                    fullscreenDialog: false,
+                  ),
+                );
+              },
+              child: Text(
+                'Cardio',
+                style: GoogleFonts.robotoSlab(
+                    fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
