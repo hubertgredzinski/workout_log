d8639800550b81bbebaed655177b6b85cd87a823
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-02-05 09:58:39 +0100

    Add cloud_firestore to history_cardio_training

--- lib/app/features/history/history_cardio_page.dart
+++ lib/app/features/history/history_cardio_page.dart
@@ -1,23 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class HistoryCardioTrainingPage extends StatefulWidget {
+class HistoryCardioTrainingPage extends StatelessWidget {
   const HistoryCardioTrainingPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<HistoryCardioTrainingPage> createState() =>
-      _HistoryCardioTrainingPageState();
-}
-
-class _HistoryCardioTrainingPageState extends State<HistoryCardioTrainingPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Text('Historia treningów siłowych'),
       appBar: AppBar(
         title: const Text('Cardio Training History'),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('cardio').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            const Text('Wystąpił błąd w aplikacji');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            const Text('Proszę czekać, trwa ładowanie danych');
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Dismissible(
+                  key: ValueKey(document.id),
+                  onDismissed: (_) {
+                    FirebaseFirestore.instance
+                        .collection('cardio')
+                        .doc(document.id)
+                        .delete();
+                  },
+                  child: TrainingCardioHistory(
+                    document['type'],
+                  ),
+                ),
+              ],
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TrainingCardioHistory extends StatelessWidget {
+  const TrainingCardioHistory(
+    this.title, {
+    super.key,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(title),
     );
   }
 }
