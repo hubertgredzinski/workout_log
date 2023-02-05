26675c530999bd889af0f2a72f82397dea83b3ac
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-02-05 09:58:11 +0100

    Add cloud_firestore to history_strength_training

--- lib/app/features/history/history_strength_training_page.dart
+++ lib/app/features/history/history_strength_training_page.dart
@@ -1,24 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class HistoryStrengthTrainingPage extends StatefulWidget {
+class HistoryStrengthTrainingPage extends StatelessWidget {
   const HistoryStrengthTrainingPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<HistoryStrengthTrainingPage> createState() =>
-      _HistoryStrengthTrainingPageState();
-}
-
-class _HistoryStrengthTrainingPageState
-    extends State<HistoryStrengthTrainingPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Text('Historia treningów siłowych'),
       appBar: AppBar(
         title: const Text('Strength Training History'),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('strength').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            const Text('Wystąpił błąd w aplikacji');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            const Text('Proszę czekać, trwa ładowanie danych');
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Dismissible(
+                  key: ValueKey(document.id),
+                  onDismissed: (_) {
+                    FirebaseFirestore.instance
+                        .collection('strength')
+                        .doc(document.id)
+                        .delete();
+                  },
+                  child: TrainingStrengthHistory(
+                    document['exercise'],
+                  ),
+                ),
+              ],
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TrainingStrengthHistory extends StatelessWidget {
+  const TrainingStrengthHistory(
+    this.title, {
+    super.key,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(title),
     );
   }
 }
