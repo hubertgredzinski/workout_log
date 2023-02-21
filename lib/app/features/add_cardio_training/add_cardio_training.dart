0d6410ad5a7343392834c56a75e21f1a0184ce8a
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-02-21 21:10:03 +0100

    Create add cardio training page

--- lib/app/features/add_cardio_training/add_cardio_training.dart
+++ lib/app/features/add_cardio_training/add_cardio_training.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddCardioTrainingPage extends StatefulWidget {
@@ -10,14 +11,126 @@ class AddCardioTrainingPage extends StatefulWidget {
 }
 
 class _AddCardioTrainingPageState extends State<AddCardioTrainingPage> {
+  var type = '';
+  var time = '';
+  var intensity = '';
+  var kcal = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add Cardio Training"),
-        actions: const [],
       ),
-      body: const Text('elo'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: ListView(
+          children: [
+            const SizedBox(height: 50),
+            TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                    borderSide:
+                        const BorderSide(color: Colors.white12, width: 3),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide:
+                        const BorderSide(color: Colors.orange, width: 3),
+                  ),
+                  hintText: 'Write type of training e.g.: Running'),
+              onChanged: (newValue) {
+                setState(() {
+                  type = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                    borderSide:
+                        const BorderSide(color: Colors.white12, width: 3),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide:
+                        const BorderSide(color: Colors.orange, width: 3),
+                  ),
+                  hintText: 'Write time of training e.g.: 10 minutes'),
+              onChanged: (newValue) {
+                setState(
+                  () {
+                    time = newValue;
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                    borderSide:
+                        const BorderSide(color: Colors.white12, width: 3),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide:
+                        const BorderSide(color: Colors.orange, width: 3),
+                  ),
+                  hintText: 'Write intensity of training e.g.: 10 km/h'),
+              onChanged: (newValue) {
+                setState(
+                  () {
+                    intensity = newValue;
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(13),
+                    borderSide:
+                        const BorderSide(color: Colors.white12, width: 3),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide:
+                        const BorderSide(color: Colors.orange, width: 3),
+                  ),
+                  hintText: 'Write number of calories burned'),
+              onChanged: (newValue) {
+                setState(
+                  () {
+                    kcal = newValue;
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+              onPressed: type.isEmpty || time.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('cardio').add(
+                        {
+                          'type': type,
+                          'time': time,
+                          'intensity': intensity,
+                          'kcal': kcal,
+                        },
+                      );
+                    },
+              child: const Text('Add training'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
