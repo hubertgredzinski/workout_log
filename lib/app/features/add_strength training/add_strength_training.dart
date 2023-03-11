37ce5b6d5ccca547409495b60a743bf76da5f96e
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-03-11 16:27:15 +0100

    Changes names of variables and move firebase to cubit in strength training

--- lib/app/features/add_strength training/add_strength_training.dart
+++ lib/app/features/add_strength training/add_strength_training.dart
@@ -1,5 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:workout_log/app/features/add_strength%20training/cubit/add_strength_training_cubit.dart';
 
 class AddStrengthTrainingPage extends StatefulWidget {
   const AddStrengthTrainingPage({
@@ -12,155 +13,155 @@ class AddStrengthTrainingPage extends StatefulWidget {
 }
 
 class _AddStrengthTrainingPageState extends State<AddStrengthTrainingPage> {
-  var exercise = '';
-  var bodyPart = '';
-  var reps = '';
-  var sets = '';
-  var weight = '';
+  String? exercise;
+  String? bodyPart;
+  String? reps;
+  String? sets;
+  String? weight;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Add Strength Training"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: ListView(
-          children: [
-            const SizedBox(height: 50),
-            TextField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(13),
-                    borderSide:
-                        const BorderSide(color: Colors.white12, width: 3),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide:
-                        const BorderSide(color: Colors.orange, width: 3),
-                  ),
-                  hintText: 'Write Exercise e.g.: Squats'),
-              onChanged: (newValue) {
-                setState(
-                  () {
-                    exercise = newValue;
-                  },
-                );
-              },
+    return BlocProvider(
+      create: (context) => AddStrengthTrainingCubit(),
+      child: BlocBuilder<AddStrengthTrainingCubit, AddStrengthTrainingState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("Add Strength Training"),
             ),
-            const SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(13),
-                    borderSide:
-                        const BorderSide(color: Colors.white12, width: 3),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide:
-                        const BorderSide(color: Colors.orange, width: 3),
-                  ),
-                  hintText: 'Write part of working body e.g.: Legs'),
-              onChanged: (newValue) {
-                setState(
-                  () {
-                    bodyPart = newValue;
-                  },
-                );
-              },
-            ),
-            const SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(13),
-                    borderSide:
-                        const BorderSide(color: Colors.white12, width: 3),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide:
-                        const BorderSide(color: Colors.orange, width: 3),
-                  ),
-                  hintText: 'Write number of sets e.g.: 3'),
-              onChanged: (newValue) {
-                setState(
-                  () {
-                    sets = newValue;
-                  },
-                );
-              },
-            ),
-            const SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(13),
-                    borderSide:
-                        const BorderSide(color: Colors.white12, width: 3),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide:
-                        const BorderSide(color: Colors.orange, width: 3),
-                  ),
-                  hintText: 'Write number of reps e.g.: 8'),
-              onChanged: (newValue) {
-                setState(
-                  () {
-                    reps = newValue;
-                  },
-                );
-              },
-            ),
-            const SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(13),
-                    borderSide:
-                        const BorderSide(color: Colors.white12, width: 3),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide:
-                        const BorderSide(color: Colors.orange, width: 3),
-                  ),
-                  hintText: ' Write reps weight e.g.: 30 kg'),
-              onChanged: (newValue) {
-                setState(
-                  () {
-                    weight = newValue;
-                  },
-                );
-              },
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
-              onPressed: exercise.isEmpty ||
-                      bodyPart.isEmpty ||
-                      reps.isEmpty ||
-                      sets.isEmpty ||
-                      weight.isEmpty
-                  ? null
-                  : () {
-                      FirebaseFirestore.instance.collection('strength').add(
-                        {
-                          'exercise': exercise,
-                          'bodypart': bodyPart,
-                          'reps': reps,
-                          'sets': sets,
-                          'weight': weight,
+            body: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ListView(
+                children: [
+                  const SizedBox(height: 50),
+                  TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              const BorderSide(color: Colors.white12, width: 3),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide:
+                              const BorderSide(color: Colors.orange, width: 3),
+                        ),
+                        hintText: 'Write exercise e.g.: Squats'),
+                    onChanged: (newValue) {
+                      setState(
+                        () {
+                          exercise = newValue;
                         },
                       );
                     },
-              child: const Text('Add training'),
-            )
-          ],
-        ),
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              const BorderSide(color: Colors.white12, width: 3),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide:
+                              const BorderSide(color: Colors.orange, width: 3),
+                        ),
+                        hintText: 'Write part of working body e.g.: Legs'),
+                    onChanged: (newValue) {
+                      setState(
+                        () {
+                          bodyPart = newValue;
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              const BorderSide(color: Colors.white12, width: 3),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide:
+                              const BorderSide(color: Colors.orange, width: 3),
+                        ),
+                        hintText: 'Write number of sets e.g.: 3'),
+                    onChanged: (newValue) {
+                      setState(
+                        () {
+                          sets = newValue;
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              const BorderSide(color: Colors.white12, width: 3),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide:
+                              const BorderSide(color: Colors.orange, width: 3),
+                        ),
+                        hintText: 'Write number of reps e.g.: 8'),
+                    onChanged: (newValue) {
+                      setState(
+                        () {
+                          reps = newValue;
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(13),
+                          borderSide:
+                              const BorderSide(color: Colors.white12, width: 3),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide:
+                              const BorderSide(color: Colors.orange, width: 3),
+                        ),
+                        hintText: ' Write reps weight e.g.: 30 kg'),
+                    onChanged: (newValue) {
+                      setState(
+                        () {
+                          weight = newValue;
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 50),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange),
+                    onPressed: exercise == null ||
+                            bodyPart == null ||
+                            reps == null ||
+                            sets == null
+                        ? null
+                        : () {
+                            context.read<AddStrengthTrainingCubit>().add(
+                                exercise!, bodyPart!, reps!, sets!, weight);
+                          },
+                    child: const Text('Add training'),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
