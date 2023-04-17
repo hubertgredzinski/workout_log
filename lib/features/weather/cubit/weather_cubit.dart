1f47e82d09ad9431e262a8ffc2d79972b04ce2ff
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-04-17 17:56:18 +0200

    Add weather model and repository

--- lib/features/weather/cubit/weather_cubit.dart
+++ lib/features/weather/cubit/weather_cubit.dart
@@ -4,5 +4,5 @@ import 'package:meta/meta.dart';
 part 'weather_state.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit() : super(WeatherInitial());
+  WeatherCubit() : super((WeatherState()));
 }
--- lib/features/weather/cubit/weather_state.dart
+++ lib/features/weather/cubit/weather_state.dart
@@ -1,6 +1,4 @@
 part of 'weather_cubit.dart';
 
 @immutable
-abstract class WeatherState {}
-
-class WeatherInitial extends WeatherState {}
+class WeatherState {}
