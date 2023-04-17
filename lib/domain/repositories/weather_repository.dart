284af323c750d2485b8114641f1018b0e5026d99
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-04-17 20:48:18 +0200

    Change dio to map string dynamic and add response in repository

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -5,9 +5,18 @@ class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    final response = await Dio().get(
-        'http://api.weatherapi.com/v1/current.json?key=c7f5e7160a0a4f7291281603223006&q=Wroclaw&aqi=no');
-    response.data;
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=c7f5e7160a0a4f7291281603223006&q=$city&aqi=no');
+    final responseData = response.data;
+    if (responseData == null) {
+      return null;
+    }
+
+    final name = responseData['location']['name'] as String;
+    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
+    return WeatherModel(
+      city: name,
+      temperature: temperature,
+    );
   }
 }
