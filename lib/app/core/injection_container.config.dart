d66a89006e65aea02bba89be4923d7861655cdb5
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-04-18 18:48:09 +0200

    -

--- lib/app/core/injection_container.config.dart
+++ lib/app/core/injection_container.config.dart
@@ -16,11 +16,6 @@ extension GetItInjectableX on _i1.GetIt {
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i2.GetItHelper(
-      this,
-      environment,
-      environmentFilter,
-    );
     return this;
   }
 }
