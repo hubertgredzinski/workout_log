b7717d2d9eb2d05c3c25b4d11e169d66f9f77e02
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-04-20 19:13:39 +0200

    Implement freezed to cardio history model

--- lib/domain/models/cardio_history_model.dart
+++ lib/domain/models/cardio_history_model.dart
@@ -1,22 +1,38 @@
 import 'package:intl/intl.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'cardio_history_model.freezed.dart';
 
-class CardioHistoryModel {
-  CardioHistoryModel({
-    required this.id,
-    required this.type,
-    required this.time,
-    required this.date,
-    required this.intensity,
-    required this.kcal,
-  });
-  final String id;
-  final String type;
-  final String time;
-  final DateTime date;
-  final String intensity;
-  final String kcal;
-
+@freezed
+class CardioHistoryModel with _$CardioHistoryModel {
+  const CardioHistoryModel._();
+  factory CardioHistoryModel({
+    required String id,
+    required String type,
+    required String time,
+    required DateTime date,
+    required String intensity,
+    required String kcal,
+  }) = _CardioHistoryModel;
   String dateFromatted() {
     return DateFormat.yMMMMEEEEd().format(date);
   }
 }
+
+// class CardioHistoryModel {
+//   CardioHistoryModel({
+//     required this.id,
+//     required this.type,
+//     required this.time,
+//     required this.date,
+//     required this.intensity,
+//     required this.kcal,
+//   });
+//   final String id;
+//   final String type;
+//   final String time;
+//   final DateTime date;
+//   final String intensity;
+//   final String kcal;
+
+
+// }
