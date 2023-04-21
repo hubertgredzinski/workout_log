e2e6d020e8a3563dce0e7c7ac05c1ca835639dc7
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-04-21 18:48:07 +0200

    Implement freezed to add cardio training state

--- lib/features/add_cardio_training/cubit/add_cardio_training_cubit.dart
+++ lib/features/add_cardio_training/cubit/add_cardio_training_cubit.dart
@@ -1,14 +1,14 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:workout_log/domain/repositories/cardio_history_repository.dart';
-
 import '../../../app/core/enums/enums.dart';
+part 'add_cardio_training_cubit.freezed.dart';
 part 'add_cardio_training_state.dart';
 
 class AddCardioTrainingCubit extends Cubit<AddCardioTrainingState> {
   AddCardioTrainingCubit(this._cardioRepository)
       : super(
-          const AddCardioTrainingState(),
+           AddCardioTrainingState(),
         );
 
   final CardioRepository _cardioRepository;
@@ -22,7 +22,7 @@ class AddCardioTrainingCubit extends Cubit<AddCardioTrainingState> {
     try {
       await _cardioRepository.add(type, time, date, intensity, kcal);
       emit(
-        const AddCardioTrainingState(
+         AddCardioTrainingState(
           status: Status.success,
         ),
       );
