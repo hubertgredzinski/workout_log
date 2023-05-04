68aeda8bcb3a2c35dd4b76a099c6d118fb7781ea
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-05-04 14:40:06 +0200

    Modify injectable for retrofit

--- lib/data/weather_remote_data_source.dart
+++ lib/data/weather_remote_data_source.dart
@@ -1,17 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import '../domain/models/weather_model.dart';
 part 'weather_remote_data_source.g.dart';
 
-@RestApi(baseUrl: "http://api.weatherapi.com/v1")
+@injectable
+@RestApi()
 abstract class WeatherRetrofitRemoteDataSource {
-  factory WeatherRetrofitRemoteDataSource(
-    Dio dio, {
-    String baseUrl,
-  }) = _WeatherRetrofitRemoteDataSource;
+  @factoryMethod
+  factory WeatherRetrofitRemoteDataSource(Dio dio) =
+      _WeatherRetrofitRemoteDataSource;
 
-  @GET("current.json?key=c7f5e7160a0a4f7291281603223006&q=/{city}&aqi=no")
-  Future<WeatherModel> getWeatherRemoteDataSource(
+  @GET("/current.json?key=c7f5e7160a0a4f7291281603223006&q=/{city}&aqi=no")
+  Future<WeatherModel?> getWeatherRemoteDataSource(
       {@Path("city") required String city});
 }
 
--- lib/data/weather_remote_data_source.g.dart
+++ lib/data/weather_remote_data_source.g.dart
@@ -13,35 +13,34 @@ class _WeatherRetrofitRemoteDataSource
   _WeatherRetrofitRemoteDataSource(
     this._dio, {
     this.baseUrl,
-  }) {
-    baseUrl ??= 'http://api.weatherapi.com/v1';
-  }
+  });
 
   final Dio _dio;
 
   String? baseUrl;
 
   @override
-  Future<WeatherModel> getWeatherRemoteDataSource(
+  Future<WeatherModel?> getWeatherRemoteDataSource(
       {required String city}) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     final Map<String, dynamic>? _data = null;
     final _result = await _dio
-        .fetch<Map<String, dynamic>>(_setStreamType<WeatherModel>(Options(
+        .fetch<Map<String, dynamic>?>(_setStreamType<WeatherModel>(Options(
       method: 'GET',
       headers: _headers,
       extra: _extra,
     )
             .compose(
               _dio.options,
-              'current.json?key=c7f5e7160a0a4f7291281603223006&q=/${city}&aqi=no',
+              '/current.json?key=c7f5e7160a0a4f7291281603223006&q=/${city}&aqi=no',
               queryParameters: queryParameters,
               data: _data,
             )
             .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
-    final value = WeatherModel.fromJson(_result.data!);
+    final value =
+        _result.data == null ? null : WeatherModel.fromJson(_result.data!);
     return value;
   }
 
