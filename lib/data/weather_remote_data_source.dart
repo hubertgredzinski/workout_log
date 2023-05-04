7b08207de20e6af3837a8f0de6db05ced3c4b240
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-05-04 13:32:57 +0200

    Implement retrofit to weatherdatasoruce

--- lib/data/weather_remote_data_source.dart
+++ lib/data/weather_remote_data_source.dart
@@ -1,16 +1,31 @@
 import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import '../domain/models/weather_model.dart';
+part 'weather_remote_data_source.g.dart';
 
-class WeatherRemoteDataSource {
-  Future<Map<String, dynamic>?> getWeatherRemoteDataSource({
-    required String city,
-  }) async {
-    try {
-      final response = await Dio().get<Map<String, dynamic>>(
-          'http://api.weatherapi.com/v1/current.json?key=c7f5e7160a0a4f7291281603223006&q=$city&aqi=no');
-      return response.data;
-    } on DioError catch (error) {
-      throw Exception(
-          error.response?.data['error']['message'] ?? 'Unknown error  ');
-    }
-  }
+@RestApi(baseUrl: "http://api.weatherapi.com/v1")
+abstract class WeatherRetrofitRemoteDataSource {
+  factory WeatherRetrofitRemoteDataSource(
+    Dio dio, {
+    String baseUrl,
+  }) = _WeatherRetrofitRemoteDataSource;
+
+  @GET("current.json?key=c7f5e7160a0a4f7291281603223006&q=/{city}&aqi=no")
+  Future<WeatherModel> getWeatherRemoteDataSource(
+      {@Path("city") required String city});
 }
+
+// class WeatherRemoteDataSource {
+//   Future<Map<String, dynamic>?> getWeatherRemoteDataSource({
+//     required String city,
+//   }) async {
+//     try {
+//       final response = await Dio().get<Map<String, dynamic>>(
+//           'http://api.weatherapi.com/v1/current.json?key=c7f5e7160a0a4f7291281603223006&q=$city&aqi=no');
+//       return response.data;
+//     } on DioError catch (error) {
+//       throw Exception(
+//           error.response?.data['error']['message'] ?? 'Unknown error  ');
+//     }
+//   }
+// }
---
+++ lib/data/weather_remote_data_source.g.dart
@@ -0,0 +1,60 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_remote_data_source.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _WeatherRetrofitRemoteDataSource
+    implements WeatherRetrofitRemoteDataSource {
+  _WeatherRetrofitRemoteDataSource(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'http://api.weatherapi.com/v1';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<WeatherModel> getWeatherRemoteDataSource(
+      {required String city}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<WeatherModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'current.json?key=c7f5e7160a0a4f7291281603223006&q=/${city}&aqi=no',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = WeatherModel.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
