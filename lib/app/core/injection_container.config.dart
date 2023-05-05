da5368dd6d1000940583e96904ce8c3b0801b412
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-05-05 10:47:55 +0200

    Implement cardio history data source to app

--- lib/app/core/injection_container.config.dart
+++ lib/app/core/injection_container.config.dart
@@ -5,27 +5,28 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i9;
+import 'package:dio/dio.dart' as _i10;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:workout_log/app/core/injection_container.dart' as _i14;
-import 'package:workout_log/data/strength_history_data_source.dart' as _i4;
-import 'package:workout_log/data/weather_remote_data_source.dart' as _i11;
+import 'package:workout_log/app/core/injection_container.dart' as _i15;
+import 'package:workout_log/data/cardio_history_data_source.dart' as _i3;
+import 'package:workout_log/data/strength_history_data_source.dart' as _i5;
+import 'package:workout_log/data/weather_remote_data_source.dart' as _i12;
 import 'package:workout_log/domain/repositories/cardio_history_repository.dart'
-    as _i3;
+    as _i4;
 import 'package:workout_log/domain/repositories/strength_history_repository.dart'
-    as _i5;
-import 'package:workout_log/domain/repositories/weather_repository.dart'
-    as _i12;
-import 'package:workout_log/features/add_cardio_training/cubit/add_cardio_training_cubit.dart'
     as _i6;
-import 'package:workout_log/features/add_strength%20training/cubit/add_strength_training_cubit.dart'
+import 'package:workout_log/domain/repositories/weather_repository.dart'
+    as _i13;
+import 'package:workout_log/features/add_cardio_training/cubit/add_cardio_training_cubit.dart'
     as _i7;
-import 'package:workout_log/features/history_training/_strength_history/cubit/strength_history_cubit.dart'
-    as _i10;
-import 'package:workout_log/features/history_training/cardio_history/cubit/cardio_history_cubit.dart'
+import 'package:workout_log/features/add_strength%20training/cubit/add_strength_training_cubit.dart'
     as _i8;
-import 'package:workout_log/features/weather/cubit/weather_cubit.dart' as _i13;
+import 'package:workout_log/features/history_training/_strength_history/cubit/strength_history_cubit.dart'
+    as _i11;
+import 'package:workout_log/features/history_training/cardio_history/cubit/cardio_history_cubit.dart'
+    as _i9;
+import 'package:workout_log/features/weather/cubit/weather_cubit.dart' as _i14;
 
 /// ignore_for_file: unnecessary_lambdas
 /// ignore_for_file: lines_longer_than_80_chars
@@ -41,32 +42,34 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     final registerModule = _$RegisterModule();
-    gh.factory<_i3.CardioRepository>(() => _i3.CardioRepository());
-    gh.factory<_i4.StrengthDataSource>(() => _i4.StrengthDataSource());
-    gh.factory<_i5.StrengthRepository>(() => _i5.StrengthRepository(
-        strengthDataSource: gh<_i4.StrengthDataSource>()));
+    gh.factory<_i3.CardioDataSource>(() => _i3.CardioDataSource());
+    gh.factory<_i4.CardioRepository>(() =>
+        _i4.CardioRepository(cardioDataSource: gh<_i3.CardioDataSource>()));
+    gh.factory<_i5.StrengthDataSource>(() => _i5.StrengthDataSource());
+    gh.factory<_i6.StrengthRepository>(() => _i6.StrengthRepository(
+        strengthDataSource: gh<_i5.StrengthDataSource>()));
     gh.factory<String>(
       () => registerModule.baseUrl,
       instanceName: 'BaseUrl',
     );
-    gh.factory<_i6.AddCardioTrainingCubit>(
-        () => _i6.AddCardioTrainingCubit(gh<_i3.CardioRepository>()));
-    gh.factory<_i7.AddStrengthTrainingCubit>(
-        () => _i7.AddStrengthTrainingCubit(gh<_i5.StrengthRepository>()));
-    gh.factory<_i8.CardioHistoryCubit>(
-        () => _i8.CardioHistoryCubit(gh<_i3.CardioRepository>()));
-    gh.lazySingleton<_i9.Dio>(
+    gh.factory<_i7.AddCardioTrainingCubit>(
+        () => _i7.AddCardioTrainingCubit(gh<_i4.CardioRepository>()));
+    gh.factory<_i8.AddStrengthTrainingCubit>(
+        () => _i8.AddStrengthTrainingCubit(gh<_i6.StrengthRepository>()));
+    gh.factory<_i9.CardioHistoryCubit>(
+        () => _i9.CardioHistoryCubit(gh<_i4.CardioRepository>()));
+    gh.lazySingleton<_i10.Dio>(
         () => registerModule.dio(gh<String>(instanceName: 'BaseUrl')));
-    gh.factory<_i10.StrengthHistoryCubit>(() => _i10.StrengthHistoryCubit(
-        strengthRepository: gh<_i5.StrengthRepository>()));
-    gh.factory<_i11.WeatherRetrofitRemoteDataSource>(
-        () => _i11.WeatherRetrofitRemoteDataSource(gh<_i9.Dio>()));
-    gh.factory<_i12.WeatherRepository>(() => _i12.WeatherRepository(
-        weatherRemoteDataSource: gh<_i11.WeatherRetrofitRemoteDataSource>()));
-    gh.factory<_i13.WeatherCubit>(() =>
-        _i13.WeatherCubit(weatherRepository: gh<_i12.WeatherRepository>()));
+    gh.factory<_i11.StrengthHistoryCubit>(() => _i11.StrengthHistoryCubit(
+        strengthRepository: gh<_i6.StrengthRepository>()));
+    gh.factory<_i12.WeatherRetrofitRemoteDataSource>(
+        () => _i12.WeatherRetrofitRemoteDataSource(gh<_i10.Dio>()));
+    gh.factory<_i13.WeatherRepository>(() => _i13.WeatherRepository(
+        weatherRemoteDataSource: gh<_i12.WeatherRetrofitRemoteDataSource>()));
+    gh.factory<_i14.WeatherCubit>(() =>
+        _i14.WeatherCubit(weatherRepository: gh<_i13.WeatherRepository>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i14.RegisterModule {}
+class _$RegisterModule extends _i15.RegisterModule {}
