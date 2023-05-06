0bcdd44cc7a644df22ef503c3b4ad902ee2febb8
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-05-06 17:50:22 +0200

    Add in comment strength cubit and repository tests (not working)

---
+++ test/domain/repositories/strength_history_repository_test.dart
@@ -0,0 +1,74 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:mocktail/mocktail.dart';
+// import 'package:workout_log/data/strength_history_data_source.dart';
+// import 'package:workout_log/domain/models/strength_history_model.dart';
+// import 'package:workout_log/domain/repositories/strength_history_repository.dart';
+// import 'package:workout_log/firebase_options.dart';
+
+// class MockStrengthDataSource extends Mock implements StrengthDataSource {}
+
+// void main() {
+
+
+//   late StrengthRepository sut;
+//   late MockStrengthDataSource strengthDataSource;
+
+//   setUp(() {
+//     strengthDataSource = MockStrengthDataSource();
+//     sut = StrengthRepository(strengthDataSource: strengthDataSource);
+//   });
+
+//   group('getStrengthStream', () {
+//     test('should call strengthDataSource.getStrengthStream method', () {
+//         final Stream<QuerySnapshot<Map<String, dynamic>>> querySnapshot;
+//       //1
+//       when(
+//         () => strengthDataSource
+//             .getStrengthDataSource()
+//             .map((querySnapshot) => querySnapshot.docs.map(
+//                   (document) {
+//                     return StrengthHistoryModel(
+//                         bodypart: '1',
+//                         date: ('2' as Timestamp).toDate(),
+//                         exercise: '3',
+//                         reps: '4',
+//                         sets: '5',
+//                         weight: '6',
+//                         id: '7');
+//                   },
+//                 ).toList()),
+//       ).thenAnswer(
+//         (_) => querySnapshot.map(
+//           (document) {
+//             return [
+//               StrengthHistoryModel(
+//                   id: '1',
+//                   bodypart: '2',
+//                   date: ('3' as Timestamp).toDate(),
+//                   exercise: '4',
+//                   reps: '5',
+//                   sets: '6',
+//                   weight: '7')
+//             ];
+//           },
+//         ),
+//       );
+//       //2
+//       final results = sut.getStrengthStream();
+//       //3
+//       expect(
+//         results,
+//         StrengthHistoryModel(
+//             id: '1',
+//             bodypart: '2',
+//             date: ('3' as Timestamp).toDate(),
+//             exercise: '4',
+//             reps: '5',
+//             sets: '6',
+//             weight: '7'),
+//       );
+//     });
+//   });
+// }
