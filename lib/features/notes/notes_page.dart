80793591d6c442bc7e92ed788cf3b296182b5605
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-07-19 16:16:50 +0200

    Add floatingActionButton, snapshot with error and connectionstate , add loop with function onDismissed delete

--- lib/features/notes/notes_page.dart
+++ lib/features/notes/notes_page.dart
@@ -11,32 +11,58 @@ class NotesPage extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
-        onPressed: () {},
+        onPressed: () {
+          FirebaseFirestore.instance.collection('notes').add(
+            {'title': 'Super kategoria'},
+          );
+        },
         child: const Icon(
           Icons.add,
         ),
       ),
-      body: StreamBuilder<Object>(
-          stream: FirebaseFirestore.instance.collection('notes').snapshots(),
-          builder: (context, snapshot) {
-            return ListView(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    Config.version,
-                    textAlign: TextAlign.right,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('notes').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Wystąpił nieoczekiwany problem');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            const Text('Proszę czeka trwa ładowanie danych');
+          }
+
+          final documents = snapshot.data!.docs;
+          return ListView(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  Config.version,
+                  textAlign: TextAlign.right,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              for (final document in documents) ...[
+                Dismissible(
+                  key: ValueKey(
+                    document.id,
+                  ),
+                  onDismissed: (_) {
+                    FirebaseFirestore.instance
+                        .collection('notes')
+                        .doc(document.id)
+                        .delete();
+                  },
+                  child: Note(
+                    document['title'],
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Note('Kategoria 1'),
-                const Note('Kategoria 2'),
-                const Note('Kategoria 3'),
               ],
-            );
-          }),
+            ],
+          );
+        },
+      ),
     );
   }
 }
