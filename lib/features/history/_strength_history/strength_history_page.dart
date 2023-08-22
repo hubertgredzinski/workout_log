a8871a05414ef1879b2d2a9d0dc5b9e89ad253d4
Hubert Grędziński <hubert.gredzinski@gmail.com>
2023-08-22 11:54:01 +0200

    Add custom datepicker

--- lib/features/history/_strength_history/strength_history_page.dart
+++ lib/features/history/_strength_history/strength_history_page.dart
@@ -1,16 +1,25 @@
+import 'package:custom_date_range_picker/custom_date_range_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import '../../../../domain/models/strength_history_model.dart';
 import '../../../app/core/enums/enums.dart';
 import '../../../app/core/injection_container.dart';
 import 'cubit/strength_history_cubit.dart';
 
-class StrengthHistoryPage extends StatelessWidget {
+class StrengthHistoryPage extends StatefulWidget {
   const StrengthHistoryPage({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<StrengthHistoryPage> createState() => _StrengthHistoryPageState();
+}
+
+class _StrengthHistoryPageState extends State<StrengthHistoryPage> {
+  DateTime? startDate;
+  DateTime? endDate;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,6 +49,73 @@ class StrengthHistoryPage extends StatelessWidget {
               final strengthModels = state.strengthDocuments;
               return ListView(
                 children: [
+                  Material(
+                    color: Colors.transparent,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 8, right: 8, top: 4, bottom: 4),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              'Choose a date Range',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15,
+                                  color: Colors.white),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              '${startDate != null ? DateFormat("dd, MMM").format(startDate!) : '-'} / ${endDate != null ? DateFormat("dd, MMM").format(endDate!) : '-'}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: const Color.fromARGB(255, 211, 3, 3),
+                    onPressed: () {
+                      showCustomDateRangePicker(
+                        context,
+                        dismissible: true,
+                        minimumDate: DateTime.now().subtract(
+                          const Duration(days: 365),
+                        ),
+                        maximumDate: DateTime.now().add(
+                          const Duration(days: 30),
+                        ),
+                        endDate: endDate,
+                        startDate: startDate,
+                        backgroundColor: Colors.black87,
+                        primaryColor: const Color.fromARGB(255, 211, 3, 3),
+                        onApplyClick: (start, end) {
+                          setState(() {
+                            endDate = end;
+                            startDate = start;
+                          });
+                        },
+                        onCancelClick: () {
+                          setState(() {
+                            endDate = null;
+                            startDate = null;
+                          });
+                        },
+                      );
+                    },
+                    child: const Icon(Icons.calendar_today_outlined,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
                   for (final strengthModel in strengthModels) ...[
                     Dismissible(
                       key: ValueKey(strengthModel.id),
